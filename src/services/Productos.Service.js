d33a5719550559ee4175f41f9d6eb7546b076db6
AlejaReyes0627 <aleja.reyes0627@hotmail.com>
2022-11-05 13:39:42 -0500

    modificaciones x2

--- src/services/Productos.Service.js
+++ src/services/Productos.Service.js
@@ -3,9 +3,9 @@ import httpClient from "./httpClient";
 const END_POINT =  "http://localhost:9090/api/productos";
 
 
-const getAllProductos = () => httpClient.get("http://localhost:9090/api/productos/getAll"); 
+const getAllProductos = () => httpClient.get(END_POINT+"/getAll"); 
 
-const getProducto = (name) => httpClient.get("http://localhost:9090/api/productos/findByName/" + name);
+const getProducto = (name) => httpClient.get(END_POINT+"/findByName/" + name);
 
 const insertProducto = (producto) => httpClient.post(END_POINT, producto); 
 
@@ -15,7 +15,7 @@ const insertProductoConFoto = (producto) => httpClient.post(END_POINT, producto,
     }
 }); 
 
-const updateProducto = (name, producto) => httpClient.put("http://localhost:9090/api/productos/ "+ name, producto); 
+const updateProducto = (name, producto) => httpClient.put(END_POINT+"/ "+ name, producto); 
 
 const updateProductoConFoto = (id, producto) => httpClient.patch(END_POINT + "/" + id + "/foto", producto, {
     headers: {
